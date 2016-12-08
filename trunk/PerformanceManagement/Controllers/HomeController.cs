805cafc2d20a9cb2e07d69f86cddff740eb7afe9
girishdubey <dubey.girish2@gmail.com>
2016-12-08 07:12:03 +0530

    fixed

--- trunk/PerformanceManagement/Controllers/HomeController.cs
+++ trunk/PerformanceManagement/Controllers/HomeController.cs
@@ -36,7 +36,11 @@ namespace PerformanceManagement.Controllers
             return View();
         }
 
-        
+        public ActionResult Header()
+        {
+            List<PMSEntity.Model.Master.Employee> _emp = new List<PMSEntity.Model.Master.Employee>();
+            return PartialView(_emp);
+        }
 
     }
 }
